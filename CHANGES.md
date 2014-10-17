744d93ccbf9ddf30a69bafd71d923433c5a54f96
Frank Stuerzebecher <frank@hostz.at>
2014-10-17 19:41:57 +0200

    CSS bugfix: shown decimal in footer menu after Firefox update.

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 ### Änderungen für CC (Custom Code): medizin-medien.at
 
+### Version 0.2 // 17.10.2014
+
+* CSS bugfix: shown decimal in footer menu after Firefox update.
+
 #### Version 0.1 // 10.09.2014
 
 * Fügt dem Header eine Zeile CCS hinzu und macht so Grid Colums responsive.
--- cc-med-med.php
+++ cc-med-med.php
@@ -3,14 +3,24 @@
 Plugin Name: Custom Code: medizin-medien.at
 Description: Site-specific functionality for medizin-medien.at like making grid columns responsive.
 Author: Frank St&uuml;rzebecher
-Version: 0.1
+Version: 0.2
+Plugin URI: https://github.com/medizinmedien/cc-med-med-at
 GitHub Plugin URI: https://github.com/medizinmedien/cc-med-med-at
 */
 
 /**
- * Make the Column Grid on the front page responsive.
+ * CSS adjustments since this site uses no child theme.
  */
-function medmed_exchange_css() {
-	print '<style>@media screen and (max-width: 800px){div.column-grid .column{float:none;margin-left:0;margin-right:0;width:auto;text-align:center;margin-bottom: 2em;}}</style>';
+function cc_medmed_adjust_css() {
+	print '<style type="text/css">/*cc-med-med-at*/';
+
+	// Responsive column grid on the front page.
+	print '@media screen and (max-width: 800px){div.column-grid .column{float:none;margin-left:0;margin-right:0;width:auto;text-align:center;margin-bottom: 2em;}}';
+
+	// After last upgrade Firefox outputs numbered list items when list-style-type
+	// is undefined. Adjust for footer navigation.
+	print '#footer #site-info .footer-nav ul li {list-style-type: none;}';
+
+	print '</style>';
 }
-add_filter( 'wp_head', 'medmed_exchange_css' );
+add_filter( 'wp_head', 'cc_medmed_adjust_css' );
